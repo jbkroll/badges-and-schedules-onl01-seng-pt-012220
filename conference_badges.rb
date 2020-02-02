2eceafc21c455a9183348535f11356f9f4192020
Jennifer Kroll <jenniferbantakroll@gmail.com>
2020-02-02 18:50:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,8 +8,9 @@ end
 
 def assign_rooms(attendees)
   new_arr = []
-  attendees.each_with_index {|name, |}
-  
-  "Hello, #{name}! You'll be assigned to room "
+  attendees.each_with_index {|name, n|}
+  new_arr << "Hello, #{name}! You'll be assigned to room #{n+1}!"
 end
+new_arr
+
   
