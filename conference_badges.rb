6de0f3e005615f2d52ac9f88a5e206989205396d
Jennifer Kroll <jenniferbantakroll@gmail.com>
2020-02-02 19:06:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,8 +2,8 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(name)
-  name.map{ |name| batch_maker(name) }
+def batch_badge_creator(attendees)
+  name.map{ |badges| batch_maker(name) }
 end
 
 def assign_rooms(name)
