1b9e6463dcc61561306b75c476b39f65d908f0f3
Jennifer Kroll <jenniferbantakroll@gmail.com>
2020-02-02 19:16:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,9 +6,9 @@ def batch_badge_creator(name)
   name.map{ |name| batch_maker(name) }
 end
 
-def assign_rooms(attendees)
+def assign_rooms(speakers)
   new_arr = []
-  attendees.each_with_index {|name, i|}
+  speakers.each_with_index {|name, i|}
     new_arr << "Hello, #{name}! You'll be assigned to room #{i+1}!"
 end
 new_arr
